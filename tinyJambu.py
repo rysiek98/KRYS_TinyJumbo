70e99db576419c3fc57827d5423b2c6969189bed
Jagoda <jagoda.bal.stud@pw.edu.pl>
2023-01-17 13:29:49 +0100

    dodanie inicjalizacji

--- tinyJambu.py
+++ tinyJambu.py
@@ -1,6 +1,8 @@
 klen = 128
 S = [0x0]*128
 K = [0x0]*128
+N = [0] * 96
+FB_n = [0, 0, 1]
 
 # Permutacja
 def state_update(S, K, i):
@@ -9,6 +11,17 @@ def state_update(S, K, i):
         for j in range(127):
             S.insert(j, S.pop()+1)
         S[127] = feedback
+    return S
+
+
+# Initialization
+S = state_update(S, K, 1024)
+
+def nonce_init(S, K, FB_n):
+    for i in range(3):
+        S[36:39] = list(a ^ b for a, b in zip(S[36:39], FB_n))
+        S = state_update(S, K, 640)
+        S[96:128] = list(a ^ b for a, b in zip(S[96:128], N[32 * i : 32 * i + 32]))
     return S
 
 a = state_update(S, K, 2048)
