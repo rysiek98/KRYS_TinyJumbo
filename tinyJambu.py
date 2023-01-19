d340acb30cf3416ac901308b50f78e3ceee16f86
rysiek98 <m.ryszka98@gmail.com>
2023-01-19 16:40:07 +0100

    Processing associated data

--- tinyJambu.py
+++ tinyJambu.py
@@ -1,8 +1,11 @@
+
 klen = 128
 S = [0x0]*128
 K = [0x0]*128
 N = [0] * 96
-FB_n = [0, 0, 1]
+AD = [0]*32
+FB_n = [0,0,1]
+FB_ad = [0,1,1]
 
 # Permutacja
 def state_update(S, K, i):
@@ -22,6 +25,14 @@ def nonce_init(S, K, FB_n):
         S[36:39] = list(a ^ b for a, b in zip(S[36:39], FB_n))
         S = state_update(S, K, 640)
         S[96:128] = list(a ^ b for a, b in zip(S[96:128], N[32 * i : 32 * i + 32]))
+    return S
+
+# Processing associated data
+def process_associated_data(S, K, FB_ad, AD):
+    for j in range (1):
+        S[36:38] = list(a ^ b for a, b in zip(S[36:38], FB_ad))
+        S = state_update(S, K, 640)
+        S[96:127] = list(a ^ b for a, b in zip(S[96:127], AD[32*j:32*j+31]))
     return S
 
 a = state_update(S, K, 2048)
